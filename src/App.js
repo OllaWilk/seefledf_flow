497451d63a5477eb2d3bc6b4447e6540201a2424
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2022-05-26 11:11:47 +0200

    add basic scss variables and seatings

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { About, Blog, Contact, Footer, Header, Methods,Opinions, Price } from './container';
 import { Navbar } from './components';
 
+import './styles/global.scss';
 
 const App = () => {
   return (
