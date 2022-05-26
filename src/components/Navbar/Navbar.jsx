dc47e87ab200731458f8107d9f330b31fbc5605e
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2022-05-26 11:52:12 +0200

    add Navigation with side navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { AiOutlineMenu } from "react-icons/ai";
+import { HiX } from "react-icons/hi";
+import SideBar from '../SideBar/SideBar';
+
 
 import './Navbar.scss';
 
 const Navbar = () => {
 
+  const [toggle, setToggle] = useState(false);
+
+  const toggleSidebar = (event) => {
+    event.preventDefault(); 
+    setToggle(!toggle);
+  }
+
   return (
   
     <nav >
-     
+      <div className='navbar-menu' >
+        <div className='app__navbar-logo'>Anna Gidzińska</div>
+        <div onClick={toggleSidebar} >
+          {toggle ? <HiX /> : <AiOutlineMenu/> }
+        </div>
+      </div>
+      <SideBar openSidebar={toggleSidebar} animate={toggle} />
+      {/* <ul  className={'toggle ' + (toggle ? 'app__header' : 'app__slider-header')}>
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li key={item}>
+            <a href={`#${item}`} onClick={toggleSidebar}>
+              {item}
+            </a>
+          </li>
+        ))}
+      </ul> */}
     </nav>
   )
 }
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -6,10 +6,17 @@ import './SideBar.scss';
 const SideBar = ({openSidebar, animate}) => {
 
   return (
-        <>
-        sidebar
-        </>
-       
+        <ul className={'toggle ' + (animate ? 'app__menu-sidebar open ' : 'app__menu-sidebar')} >
+
+          {[ 'home', 'metody', 'o mnie', 'opinie', 'cennik', 'blog'].map((item) => (
+          <li key={item}>
+            <a href={`#${item}`} onClick={openSidebar}>
+              {item}
+            </a>
+          </li>
+        ))} 
+        
+        </ul>
   )
 }
 
