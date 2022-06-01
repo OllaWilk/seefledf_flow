ea3bf2fc6fd3496fe81da97d6b5a119788fe5bbb
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2022-06-01 18:01:20 +0200

    Updates

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as ButtonBlack } from './Button/ButtonBlack';
-export { default as ButtonWhite } from './Button/ButtonWhite';
-export { default as Navbar } from './Navbar/Navbar';
-export { default as SideBar } from './SideBar/SideBar';
-export { default as Headlines } from './Headlines/Headlines';
-export { default as PhotoCart } from './PhotoCart/PhotoCart';
