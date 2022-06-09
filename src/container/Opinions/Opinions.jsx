b3743ab7ca1a23e264c89e386265940f03b29d53
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2022-06-09 14:26:06 +0200

    replace photos and opinions. Add new opinions

--- src/container/Opinions/Opinions.jsx
+++ src/container/Opinions/Opinions.jsx
@@ -11,20 +11,32 @@ const Opinions = () => {
 
   const testimonialsArr = [ 
     {
-      name: "Katarzyna Kowalska",
-      feedback: "As I embark on the seemingly impossible task of trying to replace Olivia Marley, I realise just how lucky I was to find such a grounding, compassionate and inspiring yoga teacher in London. I’ve just relocated to Beijing and my Yoga with Olivia classes are one of the first things I’m missing (after my mum and M&S). Olivia teaches in a way that makes it accessible for everyone - when I joined her class I knew absolutely nothing about yoga - but I now have a solid understanding of physical alignment and generally the way my own body moves!"
+      name: "Radek & Karolina, menadżerowie, Polska",
+      feedback: "Całe doświadczenie z Joga&Masaż Ciała&Umysłu jest niesamowite i słowa nigdy tego nie wyrażą. Razem z moją Karoliną regularnie odwiedzaliśmy Annę na masaże tajskie i zawsze było super. Po ciężkiej kontuzji, połączonej z pewnym stresującym okresem w zbyt zabieganym życiu, doświadczam wielu napięć. Dzięki niesamowitym umiejętnościom i talentom Anny w końcu znalazłem ulgę. Aha... a po sesji zawsze cieszę się filiżanką wspaniałej herbaty i rozmową o życiu. Gorąco polecam.",
     },
     {
-      name: "Aleksandra Wilk",
-      feedback: " I just wanted to say a huge thank you for the retreat! After a really busy period it was just the ticket and perfectly balanced in terms of yoga sessions, things to do and time to ourselves. You were also an incredible host and the sunset yoga was amazing!! I felt like I got a lot out of the sessions, all of which I’m hoping to remember in classes (although I’ll put my hands up to possibly being lazy and forgetting at times! ). FYI, also going to be booking in for me and a friend for the January London retreat day :)"
+      name: "Agnieszka, grafik, Polska",
+      feedback: "Dzięki praktyce z Joga&Masaż Ciała&Umysłu czuję się o wiele lepiej, mam siłę i energię do pracy, i pokonywania codziennych trudności. Dzień rozpoczęty od ćwiczeń jest zawsze lepszy. Uspokajam się, radzę sobie ze stresem, mam ochotę działać, a radość mnie po prostu rozpiera od środka.",
     },
     {
-      name: "Wiktoria Raczyńska",
-      feedback: "I thought the yoga was great, the food was delicious, my room and the villa was absolutely delightful, and the company was lovely. Thank you for organising such a lovely week! It was much needed, massively relaxing, and I learnt loads too, as well as it being tons of fun."
+      name: "Katarzyna, biolog z Islandii",
+      feedback: "Wspaniały masaż, usuwa wszystkie blokady i zastoje w ciele i przywraca je do pełnej sprawności. Ania po jednej sesji wyleczyła mi kontuzję barku!",
     },
     {
-      name: "Jakub Królikiewicz",
-      feedback: " I just wanted to say a huge thank you for the retreat! After a really busy period it was just the ticket and perfectly balanced in terms of yoga sessions, things to do and time to ourselves. You were also an incredible host and the sunset yoga was amazing!! I felt like I got a lot out of the sessions, all of which I’m hoping to remember in classes (although I’ll put my hands up to possibly being lazy and forgetting at times! ). FYI, also going to be booking in for me and a friend for the January London retreat day :)"
+      name: "Jula, piosenkarka, Polska",
+      feedback: "Na kursie z Joga&Masaż Ciała&Umysłu nauczyłam się od Ani wspaniałej płynnej sekwencji ruchów. Ćwicząc codziennie po chociaż 15 minut można odczuć naprawdę zdumiewające efekty, ćwiczenia pomogły mi zdobyć większą świadomość i uważność ciała i oddechu, uspokoić się oraz pozbyć nadmiaru wody z organizmu ! Po niezbyt długim czasie wdrożonej w system dnia praktyki słyszałam od znajomych, że jakoś tak młodziej wyglądam i chyba schudłam ;) Polecam zarówno metodę jak i prowadzącą i jeszcze raz dziękuję za zdobytą wiedzę !",
+    },
+    {
+      name: "René, menadżer, Szwajcaria",
+      feedback: "Sesja masażu u Ani to prawdziwe przeżycie – podróż w głąb swojego wewnętrznego świata. W bardzo subtelny sposób przełamuje blokady i otwiera przestrzeń dla nowych wewnętrznych i zewnętrznych ruchów i doświadczeń.",
+    },
+    {
+      name: "Gloria, stomatolog, Szwajcaria.",
+      feedback: "Dziękuję Ani za wyjątkowe 3 dni, wprowadzenie i przewodnictwo w Joga&Masaż Ciała&Umysłu. To doświadczenie otworzyło dla mnie cudowną ścieżkę samoświadomości, którą teraz praktykuję prawie każdego dnia, kiedy tylko jest to możliwe. Ania przekazała swoją wiedzę z delikatnością i radosną jasnością, dzięki czemu moje ciało i umysł szybko nauczyły się integrować to, czego się nauczyłam. Dziękuję jej z całego serca za jej kochający sposób przekazania tego daru.",
+    },
+    {
+      name: "Marta, farmaceutka, Polska.",
+      feedback: "Fantastyczny kurs z Joga&Masaż Ciała&Umysłu, wspaniały czas. Dziękuję!",
     }
   ]
 
